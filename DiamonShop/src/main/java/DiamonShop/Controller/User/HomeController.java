612dab77f607ca85682ee5b0ecc0374268339297
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-10-21 14:52:51 +0700

    #8 hien thi du lieu dong cho slides

--- DiamonShop/src/main/java/DiamonShop/Controller/User/HomeController.java
+++ DiamonShop/src/main/java/DiamonShop/Controller/User/HomeController.java
@@ -1,14 +1,22 @@
-package DiamonShop.UserController;
+package DiamonShop.Controller.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import DiamonShop.Dao.SlidesDao;
+import DiamonShop.Service.User.HomeServiceImpl;
+
 @Controller
 public class HomeController {
+	@Autowired
+	HomeServiceImpl homeService;
+	
 	@RequestMapping(value = {"/", "/trang-chu/"})
 		public ModelAndView index() {
 			ModelAndView mv = new ModelAndView("user/index");
+			mv.addObject("slides", homeService.getDataSlide());
 			return mv;
 		}
 	
