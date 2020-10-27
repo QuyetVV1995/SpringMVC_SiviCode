f8f23072337290377e7f04da7cd75ccfb45fc298
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-10-27 14:09:28 +0700

    #25 them -sua-xoa-tinh tong gio hang

--- DiamonShop/src/main/java/DiamonShop/Dao/CartDao.java
+++ DiamonShop/src/main/java/DiamonShop/Dao/CartDao.java
@@ -1,15 +1,21 @@
 package DiamonShop.Dao;
 
 import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import DiamonShop.Dto.CartDto;
 import DiamonShop.Dto.ProductsDto;
 
+@Repository
 public class CartDao extends BaseDao {
 
+	@Autowired
 	ProductsDao productsDao = new ProductsDao();
 	
-	public CartDto addCart(long id, HashMap<Long, CartDto> cart) {
+	public HashMap<Long, CartDto> addCart(long id, HashMap<Long, CartDto> cart) {
 		CartDto itemCart = new CartDto();
 		ProductsDto product = productsDao.findProductById(id);
 		if(product != null) {
@@ -17,6 +23,50 @@ public class CartDao extends BaseDao {
 			itemCart.setQuanty(1);
 			itemCart.setTotalPrice(product.getPrice());
 		}
-		return itemCart;
+		cart.put(id, itemCart);	
+		return cart;
 	}
+	
+	public HashMap<Long, CartDto> editCart(long id, int quanty, HashMap<Long, CartDto> cart) {
+		if(cart == null) {
+			return cart;
+		}
+		CartDto itemCart = new CartDto();
+		if(cart.containsKey(id)) {
+			itemCart = cart.get(id);
+			itemCart.setQuanty(quanty);
+			itemCart.setTotalPrice(quanty * itemCart.getProduct().getPrice());
+		}
+		cart.put(id, itemCart);
+		
+		return cart;
+	}
+	
+	public HashMap<Long, CartDto> deleteCart(long id, HashMap<Long, CartDto> cart) {
+		if(cart == null) {
+			return cart;
+		}
+		if(cart.containsKey(id)) {
+			cart.remove(id);
+		}
+		return cart;
+	}
+	
+	public int totalQuanty(HashMap<Long, CartDto> cart) {
+		int totalQuanty = 0;
+		for(Map.Entry<Long, CartDto> itemCart : cart.entrySet()) {
+			totalQuanty += itemCart.getValue().getQuanty();
+		}
+		return totalQuanty;
+	}
+	
+	public double totalPrice(HashMap<Long, CartDto> cart) {
+		int totalPrice = 0;
+		for(Map.Entry<Long, CartDto> itemCart : cart.entrySet()) {
+			totalPrice += itemCart.getValue().getTotalPrice();
+		}
+		return totalPrice;
+	}
+	
+	
 }
